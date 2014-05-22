918c55f874a6035e1525593b70ecc1a686639285
Gosling <vongosling.fengj@alibaba-inc.com>
2014-05-22 18:45:00 +0800

    Optimize

--- 2-install-zookeeper.sh
+++ 2-install-zookeeper.sh
@@ -1,2 +1,7 @@
-apt-get update
-apt-get --yes install zookeeper=3.3.5* zookeeperd=3.3.5*
+wget http://mirrors.cnnic.cn/apache/zookeeper/stable/zookeeper-3.4.6.tar.gz && tar -xzvf zookeeper-3.4.6.tar.gz
+cd zookeeper-3.4.6/conf
+cp zoo_sample.cfg zoo.cfg
+cd ../bin/
+./zkServer.sh start
+
+
--- 3-install-storm.sh
+++ 3-install-storm.sh
@@ -1,24 +1,25 @@
-#!/bin/sh
+TAR_NAME="apache-storm-0.9.1-incubating.tar.gz"
+NAME="apache-storm-0.9.1-incubating"
+wget http://mirror.bit.edu.cn/apache/incubator/storm/apache-storm-0.9.1-incubating/$TAR_NAME
 
-NAME="apache-storm-0.9.1-incubating.tar.gz"
-wget http://mirror.bit.edu.cn/apache/incubator/storm/apache-storm-0.9.1-incubating/$NAME
+sudo groupadd storm
+sudo useradd --gid storm --home-dir /home/storm --create-home --shell /bin/bash storm
 
-groupadd storm
-useradd --gid storm --home-dir /home/storm --create-home --shell /bin/bash storm
+sudo mkdir /usr/share/storm
+tar -xzvf $TAR_NAME -C /usr/share/storm
+sudo chown -R storm:storm /usr/share/storm
+ln -s /usr/share/storm/$NAME/bin/storm /usr/bin/storm
 
-tar -xzvf $NAME -C /usr/share/storm
-chown -R storm:storm /usr/share/storm
-ln -s /usr/share/storm/bin/storm /usr/bin/storm
+sudo mkdir /etc/storm
+sudo chown storm:storm /etc/storm
 
-mkdir /etc/storm
-chown storm:storm /etc/storm
+mv /usr/share/storm/$NAME/conf/storm.yaml /usr/share/storm/$NAME/conf/storm_bck.yaml
+cp storm.yaml /usr/share/storm/$NAME/conf/
+cp cluster.xml /usr/share/storm/$NAME/logback/
+ln -s /usr/share/storm/$NAME/conf/storm.yaml /etc/storm/storm.yaml 
 
-rm /usr/share/storm/conf/storm.yaml
-cp storm.yaml /usr/share/storm/conf/
-cp cluster.xml /usr/share/storm/logback/
-ln -s /usr/share/storm/conf/storm.yaml /etc/storm/storm.yaml 
+sudo mkdir /var/log/storm
+sudo chown storm:storm /var/log/storm
 
-mkdir /var/log/storm
-chown storm:storm /var/log/storm
 
-#sed -i 's/${storm.home}\/logs/\/var\/log\/storm/g' /usr/share/storm/logback/cluster.xml
+#sed-i 's/${storm.home}\/logs/\/var\/log\/storm/g' /usr/share/storm/logback/cluster.xml
